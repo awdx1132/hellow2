dc8d3413cabcea9c5da32de53a8f1dd6a7c3953c
awdx1132 <51895445@qq.com>
2017-03-07 16:56:24 +0800

    Initial commit

---
+++ hellow2model/src/test/Hellow.java
@@ -0,0 +1,11 @@
+package test;
+
+/**
+ * Created by Lenovo on 2017/3/7.
+ */
+public class Hellow {
+    public static void main(String[] args){
+        System.out
+                .println("Hellow");
+    }
+}
